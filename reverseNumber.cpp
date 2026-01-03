9c3b624242fa16e1bb2ab125f8464e945706083f
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-01-03 17:32:34 +0600

    Create reverseNumber.cpp

---
+++ reverseNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int reverseNumber(int n) {
+    int rev = 0;
+    while(n > 0) {
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n/10;
+    }
+    return rev;
+}
+
+int main() {
+    cout << reverseNumber(123456789);
+}
