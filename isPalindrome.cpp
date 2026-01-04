7f77beab5ea3621f08dbad1505a25939149cca38
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-01-04 14:00:28 +0600

    Update isPalindrome.cpp

--- isPalindrome.cpp
+++ isPalindrome.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
 using namespace std;
 
-void isPalindrome(int n) {
-    int reversed = 0;
-    int original = n;
+bool isPalindrome(int n) {
+    int reversed = 0, original = n;
+   
     
     while(n != 0) {
-        int digit = n % 10;
-        reversed = reversed * 10 + digit;
+        reversed = reversed * 10 + (n % 10);
         n/=10;
     }
     
-    if(reversed == original) {
-        cout << "This is palindrome";
-    }else {
-        cout << "This is not palindrome";
-    }
+    return reversed == original;
+      
 }
 
 int main() {
-    isPalindrome(23456);
+    cout << (isPalindrome(123214) ? "Palindrome" : "Not palindrome" );
 }
+
+// #include <iostream>
+// using namespace std;
+
+// void isPalindrome(int n) {
+//     int reversed = 0;
+//     int original = n;
+    
+//     while(n != 0) {
+//         int digit = n % 10;
+//         reversed = reversed * 10 + digit;
+//         n/=10;
+//     }
+    
+//     if(reversed == original) {
+//         cout << "This is palindrome";
+//     }else {
+//         cout << "This is not palindrome";
+//     }
+// }
+
+// int main() {
+//     isPalindrome(23456);
+// }
