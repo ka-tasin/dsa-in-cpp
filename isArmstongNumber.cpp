60dbb9b6ec826c81b4e134fe558f569ebfba15be
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-01-04 16:48:48 +0600

    Update isArmstongNumber.cpp

--- isArmstongNumber.cpp
+++ isArmstongNumber.cpp
@@ -2,29 +2,59 @@
 using namespace std;
 
 bool isArmstongNumber(int n) {
-    int digitCount = 0, original = n, temp = n;
-    int result = 0;
+    int temp = n, original = n, digitCount = 0, result = 0;
     
     while(n != 0) {
         digitCount++;
-        n/=10;
+        n /= 10;
     }
     
-    for(int i = 1; i <= digitCount; i++){
+    temp = original;
+    while(temp != 0) {
         int digit = temp % 10;
-        int digitPower = 1;
-        for(int j = 1; j <= digitCount; j++){
-            // cout << "digit before" << digit2 << endl;
-            digitPower *= digit;
-            // cout << "digit after" << digit2  << endl;
+        int power = 1;
+        for(int i = 1; i <= digitCount; i++) {
+            power *= digit; 
         }
-        result += digitPower;
-        // cout << "result" << result  << endl;
+        
+        result += power;
         temp /= 10;
     }
+    
     return result == original;
 }
 
 int main() {
-    cout << isArmstongNumber(1634);
-}     
+    cout << (isArmstongNumber(163445) ? "Armstong" : "Not armstong");
+}
+
+// #include <iostream>
+// using namespace std;
+
+// bool isArmstongNumber(int n) {
+//     int digitCount = 0, original = n, temp = n;
+//     int result = 0;
+    
+//     while(n != 0) {
+//         digitCount++;
+//         n/=10;
+//     }
+    
+//     for(int i = 1; i <= digitCount; i++){
+//         int digit = temp % 10;
+//         int digitPower = 1;
+//         for(int j = 1; j <= digitCount; j++){
+//             // cout << "digit before" << digit2 << endl;
+//             digitPower *= digit;
+//             // cout << "digit after" << digit2  << endl;
+//         }
+//         result += digitPower;
+//         // cout << "result" << result  << endl;
+//         temp /= 10;
+//     }
+//     return result == original;
+// }
+
+// int main() {
+//     cout << isArmstongNumber(1634);
+// }     
