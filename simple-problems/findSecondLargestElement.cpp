f40773fb48d7d5ad6746987284b343e4f3ea5a85
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-01-06 16:05:54 +0600

    Create findSecondLargestElement.cpp

---
+++ simple-problems/findSecondLargestElement.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int findSecondLargestElement(int a[], int size) {
+    int largest = a[0], secondLargest = a[0];
+    
+    for(int i = 1; i < size; i++) {
+        if(largest < a[i]) {
+            secondLargest = largest;
+            largest = a[i];
+        }
+        else if(secondLargest < a[i] && secondLargest < largest && a[i] != largest) {
+            secondLargest = a[i];
+        }
+    }
+   
+    return secondLargest;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 45, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << findSecondLargestElement(arr, size);
+}
