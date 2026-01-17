b8b1a1436ad36744a41f4c8ac850a099f4d06564
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-01-17 12:53:16 +0600

    Update bubble-sort.cpp

--- algorithm/bubble-sort.cpp
+++ algorithm/bubble-sort.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+void bubleSort(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;   
+            }
+        }
+    }
+    for(int i = 0; i < size; i++) {
+        cout << arr[i];
+        if(i < size - 1) cout << ", ";
+    }
+}
+int main() {
+    int arr[] = {34, 3, 56, 4, 200, 55};
+    
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    bubleSort(arr, size);
+
+    return 0;
+}
