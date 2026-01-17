aa3645fc32a5055f7753da5a78ca9bf8f71afac9
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-01-17 16:18:27 +0600

    Create 2dArray.cpp

---
+++ simple-problems/2dArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    cout << arr[2][2] << endl;
+    cout << arr[1][2];
+    
+
+    return 0;
+}
