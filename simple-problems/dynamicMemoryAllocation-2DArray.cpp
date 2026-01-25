c16416acce41c6e5ced76e6c475ca8256b7b56a7
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-01-25 18:43:28 +0600

    Add dynamic memory allocation for 2D array example
    Implement dynamic memory allocation for a 2D array and display the matrix.

---
+++ simple-problems/dynamicMemoryAllocation-2DArray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 3, cols = 3;
+    
+    int **matrix = new int*[rows];
+    for(int i = 0; i < rows; i++) {
+        matrix[i] = new int[cols];
+    }
+    
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+    
+    cout << "\nMatrix:" << endl;
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    for(int i = 0; i < rows; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+    
+
+    return 0;
+}
