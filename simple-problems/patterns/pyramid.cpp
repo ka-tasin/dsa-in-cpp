1c92244305d24d877495d193aa83c309e8407b45
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-04-11 18:29:55 +0600

    Create pyramid.cpp

---
+++ simple-problems/patterns/pyramid.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void printPyramid() {
+    int rows = 5;
+    
+    for(int i = 1; i <= rows; i++) {
+        for(int j = 1; j <= rows - i; j++) {
+            cout << " ";
+        }
+        for(int k = 1; k <= (2*i - 1); k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    
+    printPyramid();
+    
+    return 0;
+}
+
+/**
+    *
+   ***
+  *****
+ *******
+*********
+
+*/
