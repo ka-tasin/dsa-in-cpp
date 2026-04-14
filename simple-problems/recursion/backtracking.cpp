db03639dd6ce6e0b0aba72a0a71432a4582f66b5
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-04-14 12:53:48 +0600

    Create backtracking.cpp

---
+++ simple-problems/recursion/backtracking.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void print(int n) {
+    if(n < 1) return;
+    print(n-1);
+    cout << n << endl;
+}
+
+int main() {
+    int n  = 10;
+    print(n);
+    
+    return 0;
+}
