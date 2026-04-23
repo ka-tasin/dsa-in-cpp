e25388e18a9d8a6834f2dc44f49d9f3be8774f72
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-04-23 11:26:14 +0600

    Implement selection sort algorithm in C++

---
+++ algorithm/sorting/selection_sort.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++) {
+        int minimum = i;
+        for(int j = i+1; j < n; j++) {
+            if(arr[j] < arr[minimum]) {
+               minimum = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[minimum];
+        arr[minimum] = temp;
+    }
+}
+
+int main() {
+	int n;
+	cin >> n;
+	int arr[n];
+	
+	for(int i = 0; i < n; i++) cin >> arr[i];
+	
+	selection_sort(arr, n);
+	
+	for(int i = 0; i < n; i++) cout << arr[i] << " ";
+
+}
