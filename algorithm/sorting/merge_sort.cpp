27892d022d4cb65e4a6093d2168fcdd448893461
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-06-09 11:49:07 +0600

    Refactor merge sort implementation for efficiency

--- algorithm/sorting/merge_sort.cpp
+++ algorithm/sorting/merge_sort.cpp
@@ -1,67 +1,61 @@
-#include <iostream>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int> arr, int low, int mid, int high) {
-    int left = low;
-    int right = mid + 1;
+
+void merge(vector<int> &arr, int low, int mid, int high) {
     vector<int> temp;
+    int left = low;
+    int right = mid+1;
     
-    while(left > mid && mid+1 > high) {
-        if(arr[left] < arr[right]) {
+    while(left <= mid && right <= high) {
+        if(arr[left] <= arr[right]) {
             temp.push_back(arr[left]);
             left++;
-        }else{
+        }
+        else{
             temp.push_back(arr[right]);
             right++;
         }
     }
     
-    while(left > mid) {
+    while(left <= mid) {
         temp.push_back(arr[left]);
         left++;
     }
-    while(mid+1 > high) {
+    while(right <= high) {
         temp.push_back(arr[right]);
         right++;
     }
     
-   for(int i = 0; i < arr.size(); i++) {
-       arr[i] = temp[i];
+    for(int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
     }
 }
 
-void mS(vector<int> arr, int low, int high) {
-    if(low >= high) return;
-    
+void mergeSort(vector<int> &arr, int low, int high) {
+    if(low>=high) return;
     int mid = (low+high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid+1, high);
     
-    mS(arr, low, mid);
-    mS(arr, mid+1, high);
     merge(arr, low, mid, high);
 }
 
-void merge_sort(vector<int> arr) {
-    if(arr.size() > 0) {
-        mS(arr, 0, arr.size());
-    }
-}
 
-int main() 
-{
+int main() {
     int n;
     cin >> n;
-    vector<int > arr;
+    
+    vector<int> arr(n);
     
     for(int i = 0; i < n; i++) {
-      int temp;
-      cin >> temp;
-      arr.push_back(temp);
+        cin >> arr[i];
     }
     
-    merge_sort(arr);
+    mergeSort(arr, 0, n-1);
     
-    for(int x: arr) {
-        cout << x << " ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
+    
 }
