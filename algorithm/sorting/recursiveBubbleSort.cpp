e2a5fd3ae4a9e09437c73478e0f55bc7ca522ada
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-06-27 17:52:36 +0600

    Add recursive bubble sort implementation
    Implement recursive bubble sort algorithm and test it.

---
+++ algorithm/sorting/recursiveBubbleSort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void recursiveBubbleSort(vector<int>& arr, int n) {
+    if(n <= 1)  return;
+    
+    bool swapped = false;
+    
+    for(int i=1; i < n; i++) {
+        if(arr[i-1] > arr[i]) {
+            swap(arr[i-1], arr[i]);
+            swapped = true;
+        }
+    }
+    
+    if(swapped == false) return;
+    
+    recursiveBubbleSort(arr, n-1);
+}
+
+int main() {
+	// Test 1: Already sorted
+    vector<int> arr = {3, 9, 2, 7, 1};
+
+    recursiveBubbleSort(arr, arr.size());
+
+    for(int i =0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+
+}
