6e8fdd4e2402d2f197083d717c1d201f3ba0d817
ka-tasin <kausarahmadtasinkat@gmail.com>
2026-07-27 11:31:59 +0600

    add find largest

--- simple-problems/findLargestNumber.cpp
+++ simple-problems/findLargestNumber.cpp
@@ -1,13 +1,50 @@
+// This is bruteforce - not optiomal
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int findLargest(vector<int> &arr, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1 - 1; j++) {
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+      
+    }
+    
+    return arr[n-1];
+}
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	
+	for(int i = 0; i < n; i++) {
+	    cin >> arr[i];
+	}
+	
+	cout << findLargest(arr, n) << endl;
+	
+	for(int i = 0; i < n; i++) {
+	    cout << arr[i] << " ";
+	}
+}
+*/
+
+
 #include <iostream>
 using namespace std;
 
-int findLargestNumber(int n[], int size) {
-    int largest = n[0];
+int findLargest(vector<int> &arr, int n) {
+    int largest = arr[0];
     
-    for(int i = 0; i < size; i++){
-        if(n[i] > largest) {
-            largest = n[i];
-        }
+    for (int i = 0; i < n; i++) {
+        if(arr[i+1] > largest){
+            largest = arr[i+1];
+        } 
     }
     
     return largest;
