66bdebb9ac9df8420e1ec5686f686cf9fbff064b
ka-tasin <kausarahmadtasinkat@gmail.com>
2026-07-27 12:28:24 +0600

    add correct second largest

--- simple-problems/findSecondLargestElement.cpp
+++ simple-problems/findSecondLargestElement.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int findSecondLargestElement(int a[], int size) {
-    int largest = a[0], secondLargest = a[0];
+int findSecondLargest(vector<int> &arr, int n) {
+    int largest = arr[0];
+    // int secondLargest = 0;
+    // int secondLargest = -1;
+    int secondLargest = INT_MIN; // most edge case handled
     
-    for(int i = 1; i < size; i++) {
-        if(largest < a[i]) {
+    for (int i = 0; i < n; i++) {
+        if(arr[i] > largest){
             secondLargest = largest;
-            largest = a[i];
-        }
-        else if(secondLargest < a[i] && a[i] != largest) {
-            secondLargest = a[i];
+            largest = arr[i];
+        } 
+        else if(arr[i] < largest && arr[i] > secondLargest ) {
+            secondLargest = arr[i];
         }
     }
-   
+    
     return secondLargest;
 }
 
